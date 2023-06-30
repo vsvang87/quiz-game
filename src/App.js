6c795a8c11367d0277c25cf834d22c2758a858a8
Visay Vang <vsvang@gmail.com>
2023-06-30 09:28:02 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { quizData } from "./components/data.js";
+import "./css/style.css";
 
 function App() {
+  // set state for questions, score, and show score
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
+  // checking to see if answer is correct
+  const handleAnswer = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+    // setting up next questions
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < quizData.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+  // restart game
+  const handleRestartGame = () => {
+    window.location.reload(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="quiz-app-container">
+      <div className="wrapper">
+        <h2>Bible Quiz</h2>
+        {showScore ? (
+          <div className="score-section">
+            You scored {score} out of {quizData.length}
+          </div>
+        ) : (
+          <>
+            <div className="question-section">
+              <div className="question-count">
+                <span>Question {currentQuestion + 1}</span>/{quizData.length}
+              </div>
+              <div className="question-text">
+                {quizData[currentQuestion].question}
+              </div>
+            </div>
+            <div className="answer-section">
+              {quizData[currentQuestion].answer.map((answer) => (
+                <button
+                  onClick={() => handleAnswer(answer.isCorrect)}
+                  className="answer-btn"
+                >
+                  {answer.answerText}
+                </button>
+              ))}
+            </div>
+          </>
+        )}
+
+        <div className="restart">
+          {currentQuestion ? (
+            <button className="restart-btn" onClick={() => handleRestartGame()}>
+              Restart
+            </button>
+          ) : (
+            ""
+          )}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/data.js
@@ -0,0 +1,213 @@
+export const quizData = [
+  {
+    question: "Who are the first two God created?",
+    answer: [
+      {
+        answerText: "Adam & Sarah",
+        isCorrect: false,
+      },
+      {
+        answerText: "Adam & Lilith",
+        isCorrect: false,
+      },
+      {
+        answerText: "Adam & Eve",
+        isCorrect: true,
+      },
+      {
+        answerText: "Adam & Lara",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    question: "Who did God used to rescue Isreal from Egypt?",
+    answer: [
+      {
+        answerText: "Jeremaiah",
+        isCorrect: false,
+      },
+      {
+        answerText: "Hosea",
+        isCorrect: false,
+      },
+      {
+        answerText: "Moses",
+        isCorrect: true,
+      },
+      {
+        answerText: "Ezra",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    question:
+      "How many years did the Isrealites spent in the desert after being rescued from Egypt?",
+    answer: [
+      {
+        answerText: "40 years",
+        isCorrect: true,
+      },
+      {
+        answerText: "45 years",
+        isCorrect: false,
+      },
+      {
+        answerText: "20 years",
+        isCorrect: true,
+      },
+      {
+        answerText: "30 years",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    question: "Who lead the Isrealites after Moses?",
+    answer: [
+      {
+        answerText: "Caleb",
+        isCorrect: false,
+      },
+      {
+        answerText: "Joshua",
+        isCorrect: true,
+      },
+      {
+        answerText: "David",
+        isCorrect: false,
+      },
+      {
+        answerText: "Aaron",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    question: "Who was the first king in Isreal?",
+    answer: [
+      {
+        answerText: "David",
+        isCorrect: false,
+      },
+      {
+        answerText: "Saul",
+        isCorrect: true,
+      },
+      {
+        answerText: "Hazakiel",
+        isCorrect: false,
+      },
+      {
+        answerText: "Absolum",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    question: "Who is king Solomon's father?",
+    answer: [
+      {
+        answerText: "Saul",
+        isCorrect: false,
+      },
+      {
+        answerText: "David",
+        isCorrect: true,
+      },
+      {
+        answerText: "Nathan",
+        isCorrect: false,
+      },
+      {
+        answerText: "Isaiah",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    question: "Who is Jesus?",
+    answer: [
+      {
+        answerText: "Healer",
+        isCorrect: false,
+      },
+      {
+        answerText: "Savior",
+        isCorrect: false,
+      },
+      {
+        answerText: "Son of God",
+        isCorrect: false,
+      },
+      {
+        answerText: "All the Above",
+        isCorrect: true,
+      },
+    ],
+  },
+  {
+    question: "Who is Peter?",
+    answer: [
+      {
+        answerText: "Stranger",
+        isCorrect: false,
+      },
+      {
+        answerText: "Apostle",
+        isCorrect: true,
+      },
+      {
+        answerText: "Roman Soldier",
+        isCorrect: false,
+      },
+      {
+        answerText: "Nobody",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    question: "How many disciples did Jesus have?",
+    answer: [
+      {
+        answerText: "10",
+        isCorrect: false,
+      },
+      {
+        answerText: "12",
+        isCorrect: true,
+      },
+      {
+        answerText: "8",
+        isCorrect: false,
+      },
+      {
+        answerText: "11",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    question: "Who are Jesus's earthly parents?",
+    answer: [
+      {
+        answerText: "Joseph & Martha",
+        isCorrect: false,
+      },
+      {
+        answerText: "Joseph & Mary",
+        isCorrect: true,
+      },
+      {
+        answerText: "Zachariah & Elizabeth",
+        isCorrect: false,
+      },
+      {
+        answerText: "Lazarus & Judith",
+        isCorrect: false,
+      },
+    ],
+  },
+];
